99cb165574bbb89f0200dbb7b60fdaa228c62b03
tomaz.kramberger <tomaz.kramberger3@um.si>
2020-01-29 11:35:37 +0100

    Added more problems and heat map visualization.

---
+++ src/problems/DE.java
@@ -0,0 +1,20 @@
+package problems;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class DE {
+    List<Solution> population = new ArrayList<>();
+    int D = 20;
+    double F = 0.4;
+    double CR = 0.4;
+
+    Solution best;
+
+    Random r = new Random();
+
+    Problem p;
+
+
+}
